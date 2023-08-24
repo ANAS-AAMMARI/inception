edc106ebd8481cb2b139ba3e6da00e23c70ac946
ANAS-AAMMARI <aaammari@student.1337.ma>
2023-08-24 13:47:51 +0100

    mariaDb container

--- mariaDb/tool/mariaDb.sh
+++ mariaDb/tool/mariaDb.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# MariaDB
-
 service mysql start
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
+mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASS}';"
 
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'localhost' IDENTIFIED BY '${USER_PASS}';"
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASSWORD';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';"
 
 mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$ROOT_PASSWORD shutdown
+mysqladmin -u root -p $ROOT_PASS shutdown
 
 exec mysqld_safe
+
+# kill $(cat /var/run/mysqld/mysqld.pid)
+
+# service mysql start
