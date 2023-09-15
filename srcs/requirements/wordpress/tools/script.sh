de85e78fb257346d20f5886b067de671f1b220aa
ANAS-AAMMARI <aaammari@student.1337.ma>
2023-09-15 15:32:03 +0100

    all work fine

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -10,29 +10,47 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 # install wordpress
-wp core download --path=wordpress
+wp core download --path=/var/www --allow-root
+
+cat << EOF > /var/www/wp-config.php
+<?php
+define( 'DB_NAME', '$DB_NAME' );
+define( 'DB_USER', '$DB_USER' );
+define( 'DB_PASSWORD', '$USER_PASS' );
+define( 'DB_HOST', 'mariadb:3306' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
+define('FS_METHOD','direct');
+\$table_prefix = '';
+define( 'WP_DEBUG', false );
+if ( ! defined( 'ABSPATH' ) ) {
+define( 'ABSPATH', __DIR__ . '/' );}
+require_once ABSPATH . 'wp-settings.php';
+EOF
 
 # create wp-config.php
-wp config create --allow-root \
-    --dbname=$DB_NAME \
-    --dbuser=$DB_USER \
-    --dbpass=$DB_PASSWORD \
-    --dbhost=mariadb:3306 \
-    --path=/var/www/wordpress
+# wp core config --allow-root \
+#     --dbname=$DB_NAME \
+#     --dbuser=$DB_USER \
+#     --dbpass=$DB_PASS \
+#     --dbhost=mariadb:3306 \
+#     --path=/var/www
 
-# create admin user for wordpress
+# # create admin user for wordpress
 wp core install --allow-root \
     --url=$WP_URL \
     --title=$WP_TITLE \
     --admin_user=$WP_ADMIN_USER \
     --admin_password=$WP_ADMIN_PASSWORD \
     --admin_email=$WP_ADMIN_EMAIL \
-    --path=/var/www/wordpress
+    --path=/var/www
 
-# create normal user for wordpress
+# # create normal user for wordpress
 wp user create --allow-root \
     $WP_USER \
     $WP_USER_EMAIL \
     --user_pass=$WP_USER_PASSWORD \
     --role=author \
-    --path=/var/www/wordpress
+    --path=/var/www
+
+php-fpm81 -F
---
+++ srcs/requirements/wordpress/tools/test.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# mkdir -p /var/www/html
+
+cd /var/www/html
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+chmod +x wp-cli.phar
+
+mv wp-cli.phar /usr/local/bin/wp
+
+wp core download --allow-root
+
+# wp core config --allow-root 
+wp config create --allow-root \
+    --dbname=$DB_NAME \
+    --dbuser=$DB_USER \
+    --dbpass=$USER_PASS \
+    --dbhost=mariadb --skip-check
+
+wp core install --allow-root \
+    --url=$WP_URL \
+    --title=$WP_TITLE \
+    --admin_user=$WP_ADMIN_USER \
+    --admin_password=$WP_ADMIN_PASS \
+    --admin_email=$WP_ADMIN_EMAIL 
+
+wp user create --allow-root \
+    $WP_USER \
+    $WP_USER_EMAIL \
+    --role=author 
+    # --user_pass=$WP_USER_PASSWORD \
+
+php-fpm81 -F
